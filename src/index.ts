34075603ed941c9b35618ff9265f60ec41e66a27
fobdev <silvapedro55@gmail.com>
2021-11-20 13:45:21 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,62 @@
+import Canvas, { createCanvas } from "canvas";
+import express from "express";
+
+const generateImage = async (topText: string, bottomText: string, imageURL: string) => {
+    const image = await Canvas.loadImage(imageURL);
+    const width = image.width;
+    const height = image.height;
+
+    const canvas = createCanvas(width, height);
+    const ctx = canvas.getContext("2d");
+
+    ctx.drawImage(image, 0, 0, width, height);
+
+    Canvas.registerFont("./src/fonts/Impact.ttf", { family: "Sans-Serif" });
+    Canvas.registerFont("./src/fonts/NotoSansJP-Bold.otf", { family: "Sans-Serif" });
+
+    const japanese: RegExp =
+        /[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]/;
+
+    ctx.font = `bold ${width! / 10}px ${
+        topText.match(japanese) || bottomText.match(japanese) ? "NotoSansJP-Bold" : "Impact"
+    }`;
+    ctx.textAlign = "center";
+
+    // Top Text
+    ctx.textBaseline = "top";
+    ctx.fillStyle = "white";
+    ctx.fillText(topText, width! / 2, 0, width);
+    ctx.fillStyle = "black";
+    ctx.strokeText(topText, width! / 2, 0, width);
+
+    // Bottom Text
+    ctx.textBaseline = "bottom";
+    ctx.fillStyle = "white";
+    ctx.fillText(bottomText, width! / 2, height!, width);
+    ctx.fillStyle = "black";
+    ctx.strokeText(bottomText, width! / 2, height!, width);
+
+    return canvas.toBuffer();
+};
+
+(async () => {
+    const app = express();
+    const port = process.env.PORT || 1337;
+
+    app.get("/:topText/:bottomText/:imageURL*", async (req, res) => {
+        const { params } = req;
+        const bottomText = params.bottomText;
+        const topText = params.topText;
+
+        // @ts-ignore
+        const url = params?.[0];
+
+        const image = await generateImage(topText, bottomText, `https:${url}`);
+
+        res.send({ image });
+    });
+
+    app.listen(port, () => {
+        console.log(`Client listening to port ${port}`);
+    });
+})();
